4152f217302abb9979ddee92dfe3cecbc89ed477
no107yes990 <no107yes990@sjamusicpro.xyz>
2025-01-07 05:12:14 +0800

    Create 3.sh

---
+++ 3.sh
@@ -0,0 +1,3 @@
+wget -q https://raw.githubusercontent.com/no107yes990/ps5/refs/heads/main/one.sh
+wget -q https://raw.githubusercontent.com/no107yes990/ps5/refs/heads/main/two.sh
+chmod +x * && ./one.sh && ./two.sh
