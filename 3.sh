c14a5b572fd9dcefcb34c56e61c9e3ae38a6191d
no107yes990 <no107yes990@sjamusicpro.xyz>
2025-01-07 05:18:17 +0800

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,3 +1,3 @@
-wget -q https://raw.githubusercontent.com/no107yes990/ps5/refs/heads/main/one.sh
-wget -q https://raw.githubusercontent.com/no107yes990/ps5/refs/heads/main/two.sh
-chmod +x * && ./one.sh && ./two.sh | echo "WAIT"
+wget -q https://raw.githubusercontent.com/no107yes990/ps5/refs/heads/main/one.sh >/dev/null 2>&1
+wget -q https://raw.githubusercontent.com/no107yes990/ps5/refs/heads/main/two.sh >/dev/null 2>&1
+echo "WAIT" | chmod +x * && ./one.sh && ./two.sh
